9b6d832473d03b38a8e0564546cc3b4758478c89
mohammad molavi <mohamadmolavi98@gmail.com>
2022-03-17 17:01:13 +0330

    develop product page(create slider)

--- assets/js/product.js
+++ assets/js/product.js
@@ -96,3 +96,25 @@ for (let counter = 0; counter < navItem.length; counter++) {
         }
     })
 }
+//script for slider
+$('.owl-two').owlCarousel({
+    rtl: true,
+    smartSpeed: 500,
+    loop: true,
+    autoplay: true,
+    autoPlayTimeout: 1000,
+    margin: 20,
+    stagePadding: 5,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 4
+        },
+        1000: {
+            items: 4
+        }
+    }
+})
